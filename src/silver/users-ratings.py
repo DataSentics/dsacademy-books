d5ec9bdd3ed452a8896565b5e6da1df2ed2f9b7e
VlsDanielaGb <VlsDanielaGb>
2022-10-07 13:17:03 +0000

    5/d EXERCISE with aggregate query
    Creating a table in silver layer with users and books rating joined for the query and the query with which country is the person who rated the most books since 2000

---
+++ src/silver/users-ratings.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+import pyspark.sql.functions as f
+
+# COMMAND ----------
+
+spark.sql("USE daniela_vlasceanu_books")
+
+# COMMAND ----------
+
+books_df = spark.table("books_joined_silver")
+users_df = spark.table("users_joined_pii_silver")
+
+df = books_df.join(users_df, "User-ID")
+# display(df)
+
+# COMMAND ----------
+
+df.createOrReplaceTempView("users_ratings_TempView")
+spark.sql(
+    "CREATE OR REPLACE TABLE users_ratings AS SELECT * FROM users_ratings_TempView"
+)
