47e11b6a6b909d16850525a2412b78a32bc4aaf1
VlsDanielaGb <VlsDanielaGb>
2022-10-20 07:43:56 +0000

    SOLVED-LINTER-AGAIN

--- src/daniela-gabriela-vlasceanu/gold/authors-pub-years-statistics.py
+++ src/daniela-gabriela-vlasceanu/gold/authors-pub-years-statistics.py
@@ -7,20 +7,21 @@ spark.sql("USE daniela_vlasceanu_books")
 
 # COMMAND ----------
 
-#CREATING THE COLUMNS FOR THE NEXT DF, WITH NUM_OF_MIN_RATINGS AND AVG_RATING_IN_TOTAL
+# CREATING THE COLUMNS FOR THE NEXT DF, WITH NUM_OF_MIN_RATINGS AND AVG_RATING_IN_TOTAL
 year_publisher_ratings = spark.table("authors_pub_years")
-intermediar_df =(
+intermediar_df = (
     year_publisher_ratings
     .groupBy("Year-Of-Publication", "Publisher")
     .agg(
-        f.avg("Number-of-ratings").cast("int").alias("min-votes-required"), 
+        f.avg("Number-of-ratings").cast("int").alias("min-votes-required"),
         f.avg("Rating-Average").alias("Avg-note")
     )
 )
 
 # COMMAND ----------
 
-#Using the IMDB rating formula to calculate,(num_ratings * avg_rating_by_year_publisher + min_num_ratings * avg_ratings_total)/(num_ratings + min_num_ratings)
+# Using the IMDB rating formula to calculate:
+# (num_ratings * avg_rating_by_year_publisher + min_num_ratings * avg_ratings_total)/(num_ratings + min_num_ratings)
 df_joined = year_publisher_ratings.join(
     intermediar_df, ["Year-Of-Publication", "Publisher"], "outer"
 )
@@ -34,7 +35,12 @@ df_final = (
         / (f.col("min-votes-required") + f.col("Number-of-ratings")),
     )
     .sort(f.desc("Rating-Books-scores"))
-    .select("Year-Of-Publication", "Publisher", "Book-Author", "Rating-Books-scores")
+    .select(
+        "Year-Of-Publication",
+        "Publisher",
+        "Book-Author",
+        "Rating-Books-scores"
+    )
 )
 
 # COMMAND ----------
