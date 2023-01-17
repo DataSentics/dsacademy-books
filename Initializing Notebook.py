c278c906affb74ace8008ff44bbbbebab56a508e
ovidiutoma1 <ovidiutoma1>
2023-01-17 15:15:19 +0000

    Flake 8 corrections applied

--- Initializing Notebook.py
+++ Initializing Notebook.py
@@ -13,9 +13,13 @@ spark.sql("CREATE DATABASE IF NOT EXISTS dbacademy_ovidiu_toma_datasentics_com_b
 spark.sql("USE dbacademy_ovidiu_toma_datasentics_com_book_task_advanced")
 
 
+# General path
+
+general_path = '@adapeuacadlakeg2dev.dfs.core.windows.net/gdc_academy_ovidiu_toma'
+
 # Raw paths
 
-raw_files = 'abfss://01rawdata@adapeuacadlakeg2dev.dfs.core.windows.net/gdc_academy_ovidiu_toma/'
+raw_files = f"abfss://01rawdata{general_path}"
 
 ratings_path = f"{raw_files}/BX-Book-Ratings.csv"
 books_path = f"{raw_files}/BX-Books.csv"
@@ -24,7 +28,7 @@ users_path = f"{raw_files}/BX-Users.csv"
 
 # Bronze paths
 
-parsed_files = 'abfss://02parseddata@adapeuacadlakeg2dev.dfs.core.windows.net/gdc_academy_ovidiu_toma/Data_Engineering_Workflow'
+parsed_files = f"abfss://02parseddata{general_path}/Data_Engineering_Workflow"
 
 ratings_bronze_path = f"{parsed_files}/ratings_bronze"
 books_bronze_path = f"{parsed_files}/books_bronze"
@@ -33,7 +37,7 @@ users_bronze_path = f"{parsed_files}/users_bronze"
 
 # Silver paths
 
-silver_files = 'abfss://03cleanseddata@adapeuacadlakeg2dev.dfs.core.windows.net/gdc_academy_ovidiu_toma/Data_Engineering_Workflow'
+silver_files = f"abfss://03cleanseddata{general_path}/Data_Engineering_Workflow"
 
 ratings_silver_path = f"{silver_files}/ratings_silver"
 books_silver_path = f"{silver_files}/books_silver"
@@ -43,15 +47,13 @@ user_ratings_path = f"{silver_files}/user_ratings"
 
 # Gold path
 
-gold_path = 'abfss://04golddata@adapeuacadlakeg2dev.dfs.core.windows.net/gdc_academy_ovidiu_toma/Data_Engineering_Workflow'
-
-
+gold_path = f"abfss://04golddata{general_path}/Data_Engineering_Workflow"
 
 # COMMAND ----------
 
 # Wilson Confidence Interval Function
 
-def wilson_lower_bound(pos, n, confidence=0.95):
+def wilson_lower_bound(pos, n, confidence = 0.95):
     """
     Function to provide lower bound of wilson score
     :param pos: No of positive ratings
@@ -74,11 +76,11 @@ udf_wilson = udf(wilson_lower_bound, t.DoubleType())
 
 # Bayesian Credibile Interval Function
 
-def bayesian_rating_products(n, confidence=0.95):
+def bayesian_rating_products(n, confidence = 0.95):
     """
-    Function to calculate wilson score for N star rating system. 
+    Function to calculate wilson score for N star rating system.
     :param n: Array having count of star ratings where ith index represent the votes for that category i.e. [3, 5, 6, 7, 10]
-    here, there are 3 votes for 1-star rating, similarly 5 votes for 2-star rating. 
+    here, there are 3 votes for 1-star rating, similarly 5 votes for 2-star rating.
     :param confidence: Confidence interval
     :return: Score
     """
@@ -90,9 +92,9 @@ def bayesian_rating_products(n, confidence=0.95):
     first_part = 0.0
     second_part = 0.0
     for k, n_k in enumerate(n):
-        first_part += (k+1)*(n[k]+1)/(N+K)
-        second_part += (k+1)*(k+1)*(n[k]+1)/(N+K)
-    score = first_part - z * math.sqrt((second_part - first_part*first_part)/(N+K+1))
+        first_part += (k + 1)*(n[k] + 1)/(N + K)
+        second_part += (k + 1) * (k + 1) * (n[k] + 1) / (N + K)
+    score = first_part - z * math.sqrt((second_part - first_part * first_part)/(N + K + 1))
     return float(score)
 
 spark.udf.register('bayesian_udf', bayesian_rating_products, t.DoubleType())
