54d8643929d68f44631657e1705e06863212647a
ovidiutoma1 <ovidiutoma1>
2023-01-17 15:20:23 +0000

    Further Flake 8 corrections

--- Initializing Notebook.py
+++ Initializing Notebook.py
@@ -53,7 +53,7 @@ gold_path = f"abfss://04golddata{general_path}/Data_Engineering_Workflow"
 
 # Wilson Confidence Interval Function
 
-def wilson_lower_bound(pos, n, confidence = 0.95):
+def wilson_lower_bound(pos, n, confidence=0.95):
     """
     Function to provide lower bound of wilson score
     :param pos: No of positive ratings
@@ -76,15 +76,17 @@ udf_wilson = udf(wilson_lower_bound, t.DoubleType())
 
 # Bayesian Credibile Interval Function
 
-def bayesian_rating_products(n, confidence = 0.95):
+def bayesian_rating_products(n, confidence=0.95):
     """
     Function to calculate wilson score for N star rating system.
-    :param n: Array having count of star ratings where ith index represent the votes for that category i.e. [3, 5, 6, 7, 10]
-    here, there are 3 votes for 1-star rating, similarly 5 votes for 2-star rating.
+    :param n: Array having count of star ratings where ith index
+    represent the votes for that category i.e. [3, 5, 6, 7, 10]
+    here, there are 3 votes for 1-star rating,
+    similarly 5 votes for 2-star rating.
     :param confidence: Confidence interval
     :return: Score
     """
-    if sum(n)==0:
+    if sum(n) == 0:
         return 0
     K = len(n)
     z = st.norm.ppf(1 - (1 - confidence) / 2)
@@ -92,7 +94,7 @@ def bayesian_rating_products(n, confidence = 0.95):
     first_part = 0.0
     second_part = 0.0
     for k, n_k in enumerate(n):
-        first_part += (k + 1)*(n[k] + 1)/(N + K)
+        first_part += (k + 1)*(n[k] + 1) / (N + K)
         second_part += (k + 1) * (k + 1) * (n[k] + 1) / (N + K)
     score = first_part - z * math.sqrt((second_part - first_part * first_part)/(N + K + 1))
     return float(score)
