a9eb3170849040c697064650798e01ad7068684c
alexandrucheciches <alexandrucheciches>
2023-01-18 14:59:48 +0000

    flake8 check01

--- src/ntbs/bronze/book_ratings_bronze.py
+++ src/ntbs/bronze/book_ratings_bronze.py
@@ -3,4 +3,5 @@
 
 # COMMAND ----------
 
-autoload_to_table(book_ratings_path, "book_ratings_bronze", bronze_book_ratings_checkpoint_path, "csv", "latin1", bronze_book_ratings_path, ";")
+autoload_to_table('''book_ratings_path, "book_ratings_bronze",
+bronze_book_ratings_checkpoint_path, "csv", "latin1", bronze_book_ratings_path, ";"''')
