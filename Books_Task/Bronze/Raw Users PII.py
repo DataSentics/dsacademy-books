55139211fbebfa50472834adb0cf69313292129f
ovidiutoma1 <ovidiutoma1>
2023-01-18 14:58:23 +0000

    Raw Users PII finished 01

---
+++ Books_Task/Bronze/Raw Users PII.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %run ../Initializing_Notebook
+
+# COMMAND ----------
+
+# Parsing raw users table
+
+autoload_to_table(users_pii_path, 'users_pii_bronze', users_pii_checkpoint_raw, users_pii_bronze_path, 'json', ';')
+
+# COMMAND ----------
+
+read_stream('users_pii_bronze')
+
+# COMMAND ----------
+
+# Assigning table to a variable
+
+df = spark.read.table('users_pii_bronze')
+
+# COMMAND ----------
+
+# Checking schema
+
+df.printSchema()
