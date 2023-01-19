539c612e5c115af513221b768211f22110661cd0
alexandrucheciches <alexandrucheciches>
2023-01-19 07:54:31 +0000

    flake8 check07

--- src/ntbs/bronze/book_ratings_bronze.py
+++ src/ntbs/bronze/book_ratings_bronze.py
@@ -7,6 +7,7 @@ autoload_to_table(book_ratings_path,
                   "book_ratings_bronze",
                   bronze_book_ratings_checkpoint_path,
                   "csv",
-                  "latin1", bronze_book_ratings_path,
+                  "latin1",
+                  bronze_book_ratings_path,
                   ";"
                   )
