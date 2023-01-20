149c677ab3cb3db071b0b3251b3c016d0297f5ef
alexandrucheciches <alexandrucheciches>
2023-01-20 11:00:25 +0000

    check flake8 15

--- src/ntbs/silver/users_silver.py
+++ src/ntbs/silver/users_silver.py
@@ -10,15 +10,14 @@ import pipelineutils.pathz as P
 
 (spark
  .table("users_bronze")
- .select(
-     f.trim(f.col("User-ID")).cast("integer").alias("USER_ID"),
-     f.trim(f.split(f.col("Location"), ",").getItem(0)).alias("CITY"),
-     f.trim(f.split(f.col("Location"), ",").getItem(1)).alias("REGION"),
-     f.trim(f.split(f.col("Location"), ",").getItem(2)).alias("COUNTRY"),
-     f.trim(f.when(((f.col("Age") > 122) | (f.col("Age") < 5)), None)
-            .otherwise(f.col("Age"))).cast("integer").alias("AGE"),
-     f.col("_rescued_data").alias("_rescued_data_users")
-        )
+ .select(f.trim(f.col("User-ID")).cast("integer").alias("USER_ID"),
+         f.trim(f.split(f.col("Location"), ",").getItem(0)).alias("CITY"),
+         f.trim(f.split(f.col("Location"), ",").getItem(1)).alias("REGION"),
+         f.trim(f.split(f.col("Location"), ",").getItem(2)).alias("COUNTRY"),
+         f.trim(f.when(((f.col("Age") > 122) | (f.col("Age") < 5)), None)
+                .otherwise(f.col("Age"))).cast("integer").alias("AGE"),
+         f.col("_rescued_data").alias("_rescued_data_users")
+         )
  .na.replace({'n/a' : None})
  .na.replace({'' : None})
  .na.replace({0 : None})
@@ -28,4 +27,4 @@ import pipelineutils.pathz as P
  .option("overwriteSchema", "true")
  .option("path", P.silver_users_path)
  .saveAsTable("users_silver")
- )
+  )
