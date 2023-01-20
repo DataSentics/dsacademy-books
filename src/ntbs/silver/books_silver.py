828fbf04b7c164168582baeb2335d456f7e29b96
alexandrucheciches <alexandrucheciches>
2023-01-20 11:05:28 +0000

    check flake8 18

--- src/ntbs/silver/books_silver.py
+++ src/ntbs/silver/books_silver.py
@@ -12,17 +12,17 @@ import pipelineutils.pathz as P
  .table("books_bronze")
  .withColumn("ISBN", f.substring("ISBN", 1, 10))
  .drop("Image-URL-S", "Image-URL-M", "Image-URL-L")
- .select(
-     f.trim(f.col("ISBN")).alias("ISBN"),
-     f.trim(f.col("Book-Title")).alias("BOOK_TITLE"),
-     f.trim(f.col("Book-Author")).alias("BOOK_AUTHOR"),
-     f.trim(f.col("Year-Of-Publication")).cast("integer").alias("YEAR_OF_PUBLICATION"),
-     f.trim(f.col("Publisher")).alias("PUBLISHER"),
-     f.col("_rescued_data").alias("_rescued_data_books")
-        )
+ .select(f.trim(f.col("ISBN")).alias("ISBN"),
+         f.trim(f.col("Book-Title")).alias("BOOK_TITLE"),
+         f.trim(f.col("Book-Author")).alias("BOOK_AUTHOR"),
+         f.trim(f.col("Year-Of-Publication")).cast("integer").alias("YEAR_OF_PUBLICATION"),
+         f.trim(f.col("Publisher")).alias("PUBLISHER"),
+         f.col("_rescued_data").alias("_rescued_data_books")
+         )
  .withColumn("YEAR_OF_PUBLICATION",
              f.when((f.col("YEAR_OF_PUBLICATION") == 0) | (f.col("YEAR_OF_PUBLICATION") > 2023),
-                    None).otherwise(f.col("YEAR_OF_PUBLICATION")))
+                    None).otherwise(f.col("YEAR_OF_PUBLICATION"))
+             )
  .dropDuplicates(["ISBN"])
  .write
  .format("delta")
@@ -30,4 +30,4 @@ import pipelineutils.pathz as P
  .option("overwriteSchema", "true")
  .option("path", P.silver_books_path)
  .saveAsTable("books_silver")
-)
+ )
