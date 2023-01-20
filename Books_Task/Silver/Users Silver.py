1453ca613782a225d6a9d7903338f1648d6ce93f
ovidiutoma1 <ovidiutoma1>
2023-01-20 08:23:27 +0000

    Linter corrections

--- Books_Task/Silver/Users Silver.py
+++ Books_Task/Silver/Users Silver.py
@@ -77,7 +77,8 @@ users_coalesced = (users_reversed
 # Importing existing countries list to compare with dataset
 # and assigning it to a list
 
-country_list = (spark.read
+country_list = (spark
+                .read
                 .option('header', True)
                 .csv(f'{raw_files}/Country_list/countries.csv')
                 .withColumn('name', f.lower('name')))
