017b4d3031c951e06f574975859cb60e219ac49b
alexandrucheciches <alexandrucheciches>
2023-01-20 11:53:39 +0000

    check flake8 19

--- src/ntbs/bronze/book_ratings_bronze.py
+++ src/ntbs/bronze/book_ratings_bronze.py
@@ -8,11 +8,11 @@ import pipelineutils.autoloader as A
 
 # COMMAND ----------
 
-A.autoload_to_table(P.book_ratings_path,
-                  "book_ratings_bronze",
-                  P.bronze_book_ratings_checkpoint_path,
-                  "csv",
-                  "latin1",
-                  P.bronze_book_ratings_path,
-                  ";"
-                  )
+A.autoload_to_table(P.book_ratings_path, 
+                    "book_ratings_bronze",
+                    P.bronze_book_ratings_checkpoint_path,
+                    "csv",
+                    "latin1",
+                    P.bronze_book_ratings_path,
+                    ";"
+                    )
