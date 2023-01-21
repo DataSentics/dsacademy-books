573fe5d752abfda7373020297eb1d634207e07b0
ovidiutoma1 <ovidiutoma1>
2023-01-21 02:32:23 +0000

    Popular_books_in_period Finished 01

---
+++ Books_Task/Gold/Popular_books_in_period.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %run ../Initializing_Notebook
+
+# COMMAND ----------
+
+spark.conf.set("fs.azure.account.key.adapeuacadlakeg2dev.dfs.core.windows.net",
+               "wA432KewaHRxET7kpSgyAAL6/6u031XV+wA0x/3P3UGbJLxNPxA30VBHO8euadaQ/Idcl+vGujvd+AStK8VTHg==")
+
+# COMMAND ----------
+
+# Creating a dataframe containing the Bayesian rating ordered books
+
+best_books_bayesian = spark.read.format('delta').load(f'{gold_path}/best_books_bayesian')
+
+# COMMAND ----------
+
+# Creating function to gather best books from certain period
+
+def popular_books_years(first_year, last_year):
+    return (best_books_bayesian
+            .filter(f.col('Year_of_publication') >= first_year)
+            .filter(f.col('Year_of_publication') <= last_year)
+            .sort(f.col('Bayesian_score').desc()))
+
+# COMMAND ----------
+
+# Best books in last 15 years
+
+best_last_15_years = popular_books_years(2008, 2023)
+
+# COMMAND ----------
+
+# Saving best_last_15_years to path
+
+best_last_15_years.write.format('delta').mode('overwrite').save(f'{gold_path}/best_last_15_years')
