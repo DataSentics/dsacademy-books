e61e3f4aad0620ba6e136ad6aec0c5f7ac9e2cdf
LucianFlorin <LucianFlorin>
2023-01-23 08:23:35 +0000

    second trial

--- A_Ingestion_Bronze_Tables/books_table.py
+++ A_Ingestion_Bronze_Tables/books_table.py
@@ -3,7 +3,8 @@
 
 # COMMAND ----------
 
-autoload_to_table(books_source, "books_bronze", books_checkpoint_raw, books_parsed_path, source_format='csv', delimiter=';')
+autoload_to_table(books_source, "books_bronze", books_checkpoint_raw,
+                  books_parsed_path, source_format='csv', delimiter=';')
 
 # COMMAND ----------
 
