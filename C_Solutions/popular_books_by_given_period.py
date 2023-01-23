aabad539947688ac5647814439825cb4ec807e51
LucianFlorin <LucianFlorin>
2023-01-23 11:24:22 +0000

    commit books by given period

--- C_Solutions/popular_books_by_given_period.py
+++ C_Solutions/popular_books_by_given_period.py
@@ -5,6 +5,7 @@
 
 from pyspark.sql.functions import col
 
+books_data_frame = spark.table("books_silver").join(spark.table("book_ratings_silver"), "ISBN")
 
 def best_book_by_period(df, start_year, end_year):
     ratings_count_per_book = df.groupBy('ISBN').count()
@@ -14,8 +15,17 @@ def best_book_by_period(df, start_year, end_year):
                         'Year-OF-Publication').avg('Book-Rating')
     return result.join(ratings_count_per_book, 'ISBN').sort(['avg(Book-Rating)', 'count'], ascending=[False, False])
 
-
-books_data_frame = spark.table("books_silver").join(
-    spark.table("book_ratings_silver"), "ISBN")
-
 display(best_book_by_period(books_data_frame, 2000, 2015))
+
+# COMMAND ----------
+
+df = best_book_by_period(books_data_frame, 2000, 2015)
+
+df_modified = df.withColumnRenamed('avg(Book-Rating)', 'Book-Rating')
+
+(df_modified.write
+ .format("delta")
+ .mode("overwrite")
+ .option("overwriteSchema", "true")
+ .option("path", f'{answer_question}/lost_publishers')
+ .saveAsTable("lost_publishers_answer"))
