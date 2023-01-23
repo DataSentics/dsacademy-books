b88f17c4f2afa131d1f8cd21bf9e81851c9b4373
LucianFlorin <LucianFlorin>
2023-01-23 08:21:14 +0000

    books_table test linter

---
+++ A_Ingestion_Bronze_Tables/books_table.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %run ../initial_notebook
+
+# COMMAND ----------
+
+autoload_to_table(books_source, "books_bronze", books_checkpoint_raw, books_parsed_path, source_format='csv', delimiter=';')
+
+# COMMAND ----------
+
+display(spark.table("books_bronze"))
