0367367f877417ebd700b702cf3841145821e5a1
LucianFlorin <LucianFlorin>
2023-01-23 10:56:22 +0000

    commit answer best books by period

--- C_Solutions/popular_books_by_given_period.py
+++ C_Solutions/popular_books_by_given_period.py
@@ -8,10 +8,9 @@ from pyspark.sql.functions import col
 def best_book_by_period(df, start_year, end_year):
     ratings_count_per_book = df.groupBy('ISBN').count()
     df = df.where((col('Year-Of-Publication') > start_year) & (col('Year-Of-Publication') < end_year))
-    result = df.groupBy('ISBN', 'Book-Title','Year-OF-Publication').avg('Book-Rating')
+    result = df.groupBy('ISBN', 'Book-Title', 'Year-OF-Publication').avg('Book-Rating')
     return result.join(ratings_count_per_book, 'ISBN').sort(['avg(Book-Rating)', 'count'], ascending=[False, False])
 
 books_data_frame = spark.table("books_silver").join(spark.table("book_ratings_silver"), "ISBN")
-    
     
 display(best_book_by_period(books_data_frame, 2000, 2015))
