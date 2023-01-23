8662383a60ad292f9040800d49cffacaf1fd5894
LucianFlorin <LucianFlorin>
2023-01-23 09:59:41 +0000

    commit books silver

--- B_Silver_Tables/Books_Silver.py
+++ B_Silver_Tables/Books_Silver.py
@@ -21,19 +21,19 @@
 
 # COMMAND ----------
 
-from pyspark.sql.functions import col, substring, trim, when, upper
+from pyspark.sql.functions import col, trim, when, upper
 
 (spark.read.table('books_bronze')
-.withColumn('Book-Title', trim(upper(col('Book-Title'))))
-.withColumn('Book-Author', upper(col('Book-Author')))
-.withColumn('Publisher', trim(upper(col('Publisher'))))
-.withColumn('Year-Of-Publication', col('Year-Of-Publication').cast('integer'))
-.withColumn('Year-Of-Publication', when((col('Year-Of-Publication') < 1376) |
-           (col('Year-Of-Publication') > 2021), None)
-           .otherwise(col('Year-Of-Publication')))
-.write
-.format("delta")
-.mode("overwrite")
-.option("overwriteSchema", "true")
-.option("path", books_cleansed_path)
-.saveAsTable("books_silver"))
+ .withColumn('Book-Title', trim(upper(col('Book-Title'))))
+ .withColumn('Book-Author', upper(col('Book-Author')))
+ .withColumn('Publisher', trim(upper(col('Publisher'))))
+ .withColumn('Year-Of-Publication', col('Year-Of-Publication').cast('integer'))
+ .withColumn('Year-Of-Publication', when((col('Year-Of-Publication') < 1376) |
+                                         (col('Year-Of-Publication') > 2021), None)
+             .otherwise(col('Year-Of-Publication')))
+ .write
+ .format("delta")
+ .mode("overwrite")
+ .option("overwriteSchema", "true")
+ .option("path", books_cleansed_path)
+ .saveAsTable("books_silver"))
