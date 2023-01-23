f17dae6562b8df98ae7da901c41233c8f398edfb
LucianFlorin <LucianFlorin>
2023-01-23 15:12:51 +0000

    more changes to the books_ratings_silver table

--- B_Silver_Tables/Book_Ratings_Silver.py
+++ B_Silver_Tables/Book_Ratings_Silver.py
@@ -8,7 +8,9 @@ from pyspark.sql.functions import trim, col
 (spark.table("book_ratings_bronze")
  .where(col("Book-Rating") != 0)
  .select(["User-ID", "ISBN", "Book-Rating"])
- .withColumn("User-ID", col('User-ID').cast("integer"))
+ .withColumn("User-ID", trim(col('User-ID').cast("integer")))
+ .withColumn("ISBN", trim(col("ISBN").cast("integer")))
+ .withColumn("Book-Rating", trim(col("Book-Rating").cast("integer")))
  .write
  .format("delta")
  .mode("overwrite")
