219ae988f21d4bb24f40b88ee13485ea2701166e
LucianFlorin <LucianFlorin>
2023-01-25 11:35:35 +0000

    initial notebook modified

--- initial_notebook.py
+++ initial_notebook.py
@@ -1,27 +1,4 @@
 # Databricks notebook source
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType
-
-df_books_rating_schema = StructType([StructField('User-ID', IntegerType(), False),
-                                     StructField('ISBN', StringType(), False),
-                                     StructField('Book-Rating', IntegerType(), False)])
-
-
-df_books_schema = StructType([StructField('ISBN', StringType(), True),
-                              StructField('Book-Title', StringType(), True),
-                              StructField('Book-Author', StringType(), True),
-                              StructField('YearOf-Publication',
-                                          IntegerType(), True),
-                              StructField('Publisher', StringType(), True),
-                              StructField('Image-URL-S', StringType(), True),
-                              StructField('Image-URL-M', StringType(), True),
-                              StructField('Image-URL-L', StringType(), True)])
-
-df_users_schema = StructType([StructField('User-ID', IntegerType(), False),
-                              StructField('Location', StringType(), True),
-                              StructField('Age', IntegerType(), True)])
-
-# COMMAND ----------
-
 spark.sql("create schema if not exists lucian_books_production;")
 spark.sql("use lucian_books_production;")
 
@@ -59,40 +36,3 @@ users_cleansed_path = f'{path_to_cleansed_storage}/users_silver'
 users_pii_cleansed_path = f'{path_to_cleansed_storage}/users_pii_silver'
 
 answer_question = f'{path_to_gold_storage}'
-
-# COMMAND ----------
-
-def autoload_to_table(data_source, table_name, checkpoint_directory, path, source_format='csv', delimiter=';'):
-    if source_format == 'csv':
-        query = (spark.readStream
-                      .format("cloudFiles")
-                      .option("cloudFiles.format", source_format)
-                      .option("sep", delimiter)
-                      .option("encoding", "latin1")
-                      .option("header", True)
-                      .option("cloudFiles.schemaLocation", checkpoint_directory)
-                      .load(data_source)
-                      .writeStream
-                      .format("delta")
-                      .option("checkpointLocation", checkpoint_directory)
-                      .option("path", path)
-                      .option("delta.columnMapping.mode", "name")
-                      .option("mergeSchema", "true")
-                      .trigger(availableNow=True)
-                      .outputMode("append")
-                      .table(table_name))
-    elif source_format == 'json':
-        query = (spark.readStream
-                      .format("cloudFiles")
-                      .option("cloudFiles.format", source_format)
-                      .option("cloudFiles.schemaLocation", checkpoint_directory)
-                      .load(data_source)
-                      .writeStream
-                      .format("delta")
-                      .option("checkpointLocation", checkpoint_directory)
-                      .option("mergeSchema", "true")
-                      .trigger(availableNow=True)
-                      .option("path", path)
-                      .outputMode("append")
-                      .table(table_name))
-    return query
