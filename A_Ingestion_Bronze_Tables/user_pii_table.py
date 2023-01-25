106189f1d8c01195a97bc1b9bb5c53940eeee94a
LucianFlorin <LucianFlorin>
2023-01-25 11:36:05 +0000

    ingestion users pii modified

--- A_Ingestion_Bronze_Tables/user_pii_table.py
+++ A_Ingestion_Bronze_Tables/user_pii_table.py
@@ -5,3 +5,19 @@
 
 autoload_to_table(users_pii_source, "users_pii_bronze", users_pii_checkpoint_raw,
                   users_pii_parsed_path, source_format='json')
+(spark.readStream
+.format("cloudFiles")
+.option("cloudFiles.format", "json")
+.option("encoding", "latin1")
+.option("header", True)
+.option("cloudFiles.schemaLocation", users_pii_checkpoint_raw)
+.load(users_pii_source)
+.writeStream
+.format("delta")
+.option("checkpointLocation", users_pii_checkpoint_raw)
+.option("path", users_pii_parsed_path)
+.option("delta.columnMapping.mode", "name")
+.option("mergeSchema", "true")
+.trigger(availableNow=True)
+.outputMode("append")
+.table("users_pii_bronze"))
