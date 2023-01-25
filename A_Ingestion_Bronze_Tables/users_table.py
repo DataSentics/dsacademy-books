881b68c1a6bc7f92a8178de3d680a48ddf739af9
LucianFlorin <LucianFlorin>
2023-01-25 11:36:23 +0000

    ingestion users table modified

--- A_Ingestion_Bronze_Tables/users_table.py
+++ A_Ingestion_Bronze_Tables/users_table.py
@@ -3,5 +3,20 @@
 
 # COMMAND ----------
 
-autoload_to_table(users_source, "users_bronze", users_checkpoint_raw,
-                  users_parsed_path, source_format='csv', delimiter=';')
+(spark.readStream
+.format("cloudFiles")
+.option("cloudFiles.format", "csv")
+.option("sep", ";")
+.option("encoding", "latin1")
+.option("header", True)
+.option("cloudFiles.schemaLocation", users_checkpoint_raw)
+.load(users_source)
+.writeStream
+.format("delta")
+.option("checkpointLocation", users_checkpoint_raw)
+.option("path", users_parsed_path)
+.option("delta.columnMapping.mode", "name")
+.option("mergeSchema", "true")
+.trigger(availableNow=True)
+.outputMode("append")
+.table("users_bronze"))
