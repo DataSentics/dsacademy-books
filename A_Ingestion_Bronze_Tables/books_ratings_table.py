f3165d5025d2e6d9652ed654968e6405a440c128
LucianFlorin <LucianFlorin>
2023-01-25 11:20:28 +0000

    changes ingestion table books ratings

--- A_Ingestion_Bronze_Tables/books_ratings_table.py
+++ A_Ingestion_Bronze_Tables/books_ratings_table.py
@@ -3,5 +3,20 @@
 
 # COMMAND ----------
 
-autoload_to_table(ratings_source, "book_ratings_bronze", ratings_checkpoint_raw,
-                  ratings_parsed_path, source_format='csv', delimiter=';')
+(spark.readStream
+ .format("cloudFiles")
+ .option("cloudFiles.format", "csv")
+ .option("sep", ";")
+ .option("encoding", "latin1")
+ .option("header", True)
+ .option("cloudFiles.schemaLocation", ratings_checkpoint_raw)
+ .load(ratings_source)
+ .writeStream
+ .format("delta")
+ .option("checkpointLocation", ratings_checkpoint_raw)
+ .option("path", ratings_parsed_path)
+ .option("delta.columnMapping.mode", "name")
+ .option("mergeSchema", "true")
+ .trigger(availableNow=True)
+ .outputMode("append")
+ .table("book_ratings_bronze"))
