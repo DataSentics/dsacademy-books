116b67e756c8bc90ecb38a97c003bfbb812f9fc4
LucianFlorin <LucianFlorin>
2023-01-25 11:21:25 +0000

    ingestion books table

--- A_Ingestion_Bronze_Tables/books_table.py
+++ A_Ingestion_Bronze_Tables/books_table.py
@@ -3,5 +3,20 @@
 
 # COMMAND ----------
 
-autoload_to_table(books_source, "books_bronze", books_checkpoint_raw,
-                  books_parsed_path, source_format='csv', delimiter=';')
+(spark.readStream
+ .format("cloudFiles")
+ .option("cloudFiles.format", "csv")
+ .option("sep", ";")
+ .option("encoding", "latin1")
+ .option("header", True)
+ .option("cloudFiles.schemaLocation", books_checkpoint_raw)
+ .load(books_source)
+ .writeStream
+ .format("delta")
+ .option("checkpointLocation", books_checkpoint_raw)
+ .option("path", books_parsed_path)
+ .option("delta.columnMapping.mode", "name")
+ .option("mergeSchema", "true")
+ .trigger(availableNow=True)
+ .outputMode("append")
+ .table("books_bronze"))
