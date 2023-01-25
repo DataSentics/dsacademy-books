e020bdd16ceff1a55fc86e7aee4431d42973fcf7
LucianFlorin <LucianFlorin>
2023-01-25 14:47:12 +0000

    refactoring group age query

--- C_Solutions/average_rating_by_gender.py
+++ C_Solutions/average_rating_by_gender.py
@@ -20,20 +20,18 @@ age_groups = udf(lambda age: '01_10' if age <= 10 else
                  '_100 +' if (age > 100) else '')
 
 
-df_users = spark.read.format('delta').load(
-    f'{path_to_cleansed_storage}/users_pii_silver')
-df_ratings = spark.read.format('delta').load(
-    f'{path_to_cleansed_storage}/book_ratings_silver')
-df_merged = df_users.join(df_ratings, "User-ID")
+df = (spark.read.format('delta').load(
+    f'{path_to_cleansed_storage}/users_pii_silver').join(spark.read.format('delta').load(
+    f'{path_to_cleansed_storage}/book_ratings_silver'), "User-ID"))
 
-df_merged = (df_merged
-             .filter(col('age').isNotNull())
-             .withColumn('Age_Group', age_groups(col("age")))
-             .groupBy('gender', 'Age_Group').agg(avg('Book-Rating').alias("Average-Book-Rating"))
-             .withColumn('Average-Book-Rating', col('Average-Book-Rating').cast('decimal(9, 2)'))
-             .sort('gender', 'Age_Group'))
+df = (df
+ .filter(col('age').isNotNull())
+ .withColumn('Age_Group', age_groups(col("age")))
+ .groupBy('gender', 'Age_Group').agg(avg('Book-Rating').alias("Average-Book-Rating"))
+ .withColumn('Average-Book-Rating', col('Average-Book-Rating').cast('decimal(9, 2)'))
+ .sort('gender', 'Age_Group'))
 
-display(df_merged)
+display(df)
 
 
 # COMMAND ----------
@@ -51,9 +49,11 @@ display(df_merged)
 #                                   .otherwise(when(col("age") < 41, lit("31-40"))
 #                                   .otherwise(when(col("age") < 51, lit("41-50"))
 #                                   .otherwise(when(col("age") < 61, lit("51-60"))
+#                                   .otherwise(when(col("age") < 71, lit("61-70"))
+#                                   .otherwise(when(col("age") < 81, lit("71-80"))
 #                                   .otherwise(when(col("age") < 91, lit("81-90"))
 #                                   .otherwise(when(col("age") < 101, lit("91-100"))
-#                                   .otherwise(when(col("age") > 100, lit("_101+")))))))))))
+#                                   .otherwise(when(col("age") > 100, lit("_101+")))))))))))))
 #                       .groupBy('gender', 'Age_Group').agg(avg('Book-Rating').alias("Average-Book-Rating"))
 #                       .withColumn('Average-Book-Rating', col('Average-Book-Rating').cast('decimal(9, 2)'))
 #                       .sort('gender', 'Age_Group'))
